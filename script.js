92b46ba8b652e2c6fb0c4a85d6e4f07bba52c66b
tbartsctr <tbartsctr@gmail.com>
2024-12-15 17:33:38 -0500

    SBA316Com4

--- script.js
+++ script.js
@@ -1,7 +1,45 @@
 const boxes = document.querySelectorAll(".ticTacGrid div")
 
 let currentPlayer = "X";
-let nextPlayer = "O"
+let nextPlayer = "O";
+let gameOver = "false";
+
+
+const board = [
+    ["","",""]
+    ["","",""]
+    ["","",""]
+    
+]
+
+
+function checkWinner(){
+
+    // Checks Rows
+
+    for (let i = 0; i < 3; i++)
+        if (board[i][0] && board[i][0] === board[i][1] && board[i][0] === board[i][2]) {
+
+                return `${board[i][0]} Won!`;
+        }
+}
+
+    //Checks Columns
+
+    for (let i = 0; i < 3; i++)
+        if (board[0][i] && board[0][i] === board[1][i] && board[0][1] === board[2][i]){; 
+
+                return `${board[0][i]} Won!`;
+
+        }
+    // Check Diagnols
+
+    if ((board[0][0] && board[0][0] === board[1][1] && board[0][0] === board[2][2]) || board[0][2] && board[0][2] === (board[1][1] && board[0][2] === board[2][0])){
+
+        return `${board[0][0]} Won!`;
+    }
+
+
 
 boxes.forEach(box =>{
     box.addEventListener("click", function(click){
