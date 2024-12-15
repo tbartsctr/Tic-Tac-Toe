68a8e41425aaa50468047bf2f48cbfb550c984b5
tbartsctr <tbartsctr@gmail.com>
2024-12-15 14:33:48 -0500

    TicTacCommit1

---
+++ app.js
@@ -0,0 +1,14 @@
+const boxes = document.QuerySelectorAll(".ticTacGrid")
+
+let currentPlayer = "X";
+let nextPlayer = "O"
+
+boxes.forEach(box =>{
+    box.addEventListener("click", function(click){
+        const box = click.target;
+
+    })
+})
+    
+
+
